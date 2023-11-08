cfbd973b01a929f8659c3c35acbe5be603f7e37e
Johana <56795357+johana1328@users.noreply.github.com>
2023-11-07 23:02:20 -0500

    Delete api-integracion directory

--- api-integracion/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
-insert into product (name, stock, price) values('Huesos', 100, 5000);
-insert into product (name, stock, price) values('Purina Felipito x 1kg', 50, 12300);
-insert into product (name, stock, price) values('Salsa comida cachorros', 25, 3000);
-insert into product (name, stock, price) values('Purina Gatos x 1kg', 50, 14000);
-insert into product (name, stock, price) values('Suchi gatos', 10, 12000);
--- api-integracion/src/test/java/edu/poli/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package edu.poli;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
